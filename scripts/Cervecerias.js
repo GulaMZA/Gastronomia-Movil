bbec1e1b01d55e1dc79f0de068ffca2444916699
nach00z <62139114+nach00z@users.noreply.github.com>
2020-09-24 13:46:05 -0300

    Add files via upload

---
+++ scripts/Cervecerias.js
@@ -0,0 +1,23 @@
+var Cervecerias ={
+"type": "FeatureCollection",
+"name": "Cervecerias",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": null, "Nombre": "El Botellón" }, "geometry": { "type": "Point", "coordinates": [ -68.847983112483178, -32.88899273137249 ] } },
+{ "type": "Feature", "properties": { "id": null, "Nombre": "Gran Ciervo" }, "geometry": { "type": "Point", "coordinates": [ -68.853017605801668, -32.892734341165685 ] } },
+{ "type": "Feature", "properties": { "id": null, "Nombre": "Barijho" }, "geometry": { "type": "Point", "coordinates": [ -68.858177255504216, -32.891786695969415 ] } },
+{ "type": "Feature", "properties": { "id": null, "Nombre": "Unión Cervecera" }, "geometry": { "type": "Point", "coordinates": [ -68.852245608680008, -32.883793600723777 ] } },
+{ "type": "Feature", "properties": { "id": null, "Nombre": "Gulinis" }, "geometry": { "type": "Point", "coordinates": [ -68.855072515685038, -32.883689502460626 ] } },
+{ "type": "Feature", "properties": { "id": null, "Nombre": "Chachingo Urban Tap" }, "geometry": { "type": "Point", "coordinates": [ -68.856447082165772, -32.883644486957607 ] } },
+{ "type": "Feature", "properties": { "id": null, "Nombre": "Hangar 52" }, "geometry": { "type": "Point", "coordinates": [ -68.852317160800055, -32.892870579399897 ] } },
+{ "type": "Feature", "properties": { "id": null, "Nombre": "Peñón del Águila" }, "geometry": { "type": "Point", "coordinates": [ -68.856293448516595, -32.89202179973072 ] } },
+{ "type": "Feature", "properties": { "id": null, "Nombre": "Brader Hops" }, "geometry": { "type": "Point", "coordinates": [ -68.851173762707106, -32.90887289418955 ] } },
+{ "type": "Feature", "properties": { "id": null, "Nombre": "Héroes Carranza" }, "geometry": { "type": "Point", "coordinates": [ -68.841008096951171, -32.918049753866576 ] } },
+{ "type": "Feature", "properties": { "id": null, "Nombre": "Hangar 52 Beer Garden" }, "geometry": { "type": "Point", "coordinates": [ -68.850965088965566, -32.931882352749206 ] } },
+{ "type": "Feature", "properties": { "id": null, "Nombre": "El Rinconcito al Paso" }, "geometry": { "type": "Point", "coordinates": [ -68.859664295216874, -32.891337786712562 ] } },
+{ "type": "Feature", "properties": { "id": null, "Nombre": "Believe Irish Pub" }, "geometry": { "type": "Point", "coordinates": [ -68.843617236633875, -32.894312923547957 ] } },
+{ "type": "Feature", "properties": { "id": null, "Nombre": "The Beer Club" }, "geometry": { "type": "Point", "coordinates": [ -68.882884274358872, -32.98534561308378 ] } },
+{ "type": "Feature", "properties": { "id": null, "Nombre": "Barijho Beer Garden" }, "geometry": { "type": "Point", "coordinates": [ -68.885967476750579, -32.990418305491744 ] } },
+{ "type": "Feature", "properties": { "id": null, "Nombre": "Antares" }, "geometry": { "type": "Point", "coordinates": [ -68.852149886779841, -32.89275564420074 ] } }
+]
+}
---
+++ scripts/Sushi.js
@@ -0,0 +1,15 @@
+var Sushi ={
+"type": "FeatureCollection",
+"name": "Sushi",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": null, "Nombre": "Sushi Club" }, "geometry": { "type": "Point", "coordinates": [ -68.849868833908701, -32.888569931586311 ] } },
+{ "type": "Feature", "properties": { "id": null, "Nombre": "Pez Globo" }, "geometry": { "type": "Point", "coordinates": [ -68.849639101349254, -32.887692173497065 ] } },
+{ "type": "Feature", "properties": { "id": null, "Nombre": "Sushi Shop" }, "geometry": { "type": "Point", "coordinates": [ -68.8527534133583, -32.892779352050916 ] } },
+{ "type": "Feature", "properties": { "id": null, "Nombre": "Omoi Sushi Nikkei" }, "geometry": { "type": "Point", "coordinates": [ -68.858844676450019, -32.955325579503238 ] } },
+{ "type": "Feature", "properties": { "id": null, "Nombre": "Omaki Sushi" }, "geometry": { "type": "Point", "coordinates": [ -68.799541802441169, -32.90006655969502 ] } },
+{ "type": "Feature", "properties": { "id": null, "Nombre": "Yoko Sushi Ciudad" }, "geometry": { "type": "Point", "coordinates": [ -68.858143624846377, -32.891285955547175 ] } },
+{ "type": "Feature", "properties": { "id": null, "Nombre": "Jengibre Sushi" }, "geometry": { "type": "Point", "coordinates": [ -68.855436245263789, -32.892262466128628 ] } },
+{ "type": "Feature", "properties": { "id": null, "Nombre": "Deziro Sushi" }, "geometry": { "type": "Point", "coordinates": [ -68.845084593813766, -32.903809583887828 ] } }
+]
+}
