496b73ea66336557fdc216b0ac6f4ff0d21f5719
nach00z <62139114+nach00z@users.noreply.github.com>
2020-10-02 10:38:37 -0300

    Add files via upload

---
+++ scripts/Pastas.js
@@ -0,0 +1,21 @@
+var Pastas ={
+"type": "FeatureCollection",
+"name": "Pastas",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": null, "Nombre": "Dantesco" }, "geometry": { "type": "Point", "coordinates": [ -68.822550760932018, -32.915960133178821 ] } },
+{ "type": "Feature", "properties": { "id": null, "Nombre": "La Marchigiana (Palmares)" }, "geometry": { "type": "Point", "coordinates": [ -68.85795637722012, -32.954487037176015 ] } },
+{ "type": "Feature", "properties": { "id": null, "Nombre": "Montecatini (Palmares)" }, "geometry": { "type": "Point", "coordinates": [ -68.858492419858862, -32.95445812178145 ] } },
+{ "type": "Feature", "properties": { "id": null, "Nombre": "Dantesco (Chacras)" }, "geometry": { "type": "Point", "coordinates": [ -68.878884056167365, -32.983540619870325 ] } },
+{ "type": "Feature", "properties": { "id": null, "Nombre": "La Parrala" }, "geometry": { "type": "Point", "coordinates": [ -68.845220099845562, -32.925522207712 ] } },
+{ "type": "Feature", "properties": { "id": null, "Nombre": "Anna Bistró" }, "geometry": { "type": "Point", "coordinates": [ -68.851128055499601, -32.883894667938655 ] } },
+{ "type": "Feature", "properties": { "id": null, "Nombre": "Marchigiana (Centro)" }, "geometry": { "type": "Point", "coordinates": [ -68.841957897501359, -32.884825084804461 ] } },
+{ "type": "Feature", "properties": { "id": null, "Nombre": "Montecatini (Centro)" }, "geometry": { "type": "Point", "coordinates": [ -68.842612635295907, -32.88446517046134 ] } },
+{ "type": "Feature", "properties": { "id": null, "Nombre": "Mamma Mía" }, "geometry": { "type": "Point", "coordinates": [ -68.851373103563091, -32.883948272202538 ] } },
+{ "type": "Feature", "properties": { "id": null, "Nombre": "El Mercadito (Centro)" }, "geometry": { "type": "Point", "coordinates": [ -68.858214347739874, -32.891673029514308 ] } },
+{ "type": "Feature", "properties": { "id": null, "Nombre": "El Mercadito (Chacras)" }, "geometry": { "type": "Point", "coordinates": [ -68.885638193415375, -32.989856417940075 ] } },
+{ "type": "Feature", "properties": { "id": null, "Nombre": "Tea&Company (Chacras)" }, "geometry": { "type": "Point", "coordinates": [ -68.885234246998337, -32.988702969047793 ] } },
+{ "type": "Feature", "properties": { "id": null, "Nombre": "Nipoti (Shopping)" }, "geometry": { "type": "Point", "coordinates": [ -68.799652079413534, -32.899748514513831 ] } },
+{ "type": "Feature", "properties": { "id": null, "Nombre": "Casa Vigil (Palmares)" }, "geometry": { "type": "Point", "coordinates": [ -68.859210979127553, -32.955218978630697 ] } }
+]
+}
---
+++ scripts/alplato.js
@@ -0,0 +1,31 @@
+var alplato ={
+"type": "FeatureCollection",
+"name": "alplato",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": null, "Nombre": "Bardot" }, "geometry": { "type": "Point", "coordinates": [ -68.849305989138045, -32.8877757698802 ] } },
+{ "type": "Feature", "properties": { "id": null, "Nombre": "Bute (Centro)" }, "geometry": { "type": "Point", "coordinates": [ -68.845557040933357, -32.888331201538293 ] } },
+{ "type": "Feature", "properties": { "id": null, "Nombre": "El Club de la Milanesa (Arístides)" }, "geometry": { "type": "Point", "coordinates": [ -68.856708642256933, -32.891933786600845 ] } },
+{ "type": "Feature", "properties": { "id": null, "Nombre": "Anna Bistró" }, "geometry": { "type": "Point", "coordinates": [ -68.851128055500155, -32.883895287326098 ] } },
+{ "type": "Feature", "properties": { "id": null, "Nombre": "El Asadito" }, "geometry": { "type": "Point", "coordinates": [ -68.856224050139303, -32.883777925703562 ] } },
+{ "type": "Feature", "properties": { "id": null, "Nombre": "Josefina Restó" }, "geometry": { "type": "Point", "coordinates": [ -68.852305674172086, -32.892727509154298 ] } },
+{ "type": "Feature", "properties": { "id": null, "Nombre": "Chachingo (Arístides)" }, "geometry": { "type": "Point", "coordinates": [ -68.856164223951907, -32.892040286495764 ] } },
+{ "type": "Feature", "properties": { "id": null, "Nombre": "Mumbai Masala" }, "geometry": { "type": "Point", "coordinates": [ -68.848769707194421, -32.892339289290049 ] } },
+{ "type": "Feature", "properties": { "id": null, "Nombre": "Fuente y Fonda" }, "geometry": { "type": "Point", "coordinates": [ -68.848578263394884, -32.891164170347217 ] } },
+{ "type": "Feature", "properties": { "id": null, "Nombre": "Cordillera Vinos y Fuegos" }, "geometry": { "type": "Point", "coordinates": [ -68.850093062458754, -32.88947621572747 ] } },
+{ "type": "Feature", "properties": { "id": null, "Nombre": "La Tía Rada" }, "geometry": { "type": "Point", "coordinates": [ -68.859953375354337, -32.912841078631381 ] } },
+{ "type": "Feature", "properties": { "id": null, "Nombre": "La Parrala" }, "geometry": { "type": "Point", "coordinates": [ -68.84521986054115, -32.92552071856371 ] } },
+{ "type": "Feature", "properties": { "id": null, "Nombre": "Tucson Steak House&bar" }, "geometry": { "type": "Point", "coordinates": [ -68.822986047480697, -32.915921671353225 ] } },
+{ "type": "Feature", "properties": { "id": null, "Nombre": "Dantesco" }, "geometry": { "type": "Point", "coordinates": [ -68.822550760932018, -32.915960133178821 ] } },
+{ "type": "Feature", "properties": { "id": null, "Nombre": "La Marchigiana (Palmares)" }, "geometry": { "type": "Point", "coordinates": [ -68.85795637722012, -32.954487037176015 ] } },
+{ "type": "Feature", "properties": { "id": null, "Nombre": "Montecatini (Palmares)" }, "geometry": { "type": "Point", "coordinates": [ -68.858492419858862, -32.95445812178145 ] } },
+{ "type": "Feature", "properties": { "id": null, "Nombre": "Casa Vigil (Palmares)" }, "geometry": { "type": "Point", "coordinates": [ -68.859212248545148, -32.955219557349508 ] } },
+{ "type": "Feature", "properties": { "id": null, "Nombre": "Club de la Milanesa (Palmares)" }, "geometry": { "type": "Point", "coordinates": [ -68.858875307457936, -32.955505495594259 ] } },
+{ "type": "Feature", "properties": { "id": null, "Nombre": "El Faro Bistro" }, "geometry": { "type": "Point", "coordinates": [ -68.842493461530523, -32.891793929940427 ] } },
+{ "type": "Feature", "properties": { "id": null, "Nombre": "Dantesco (Chacras)" }, "geometry": { "type": "Point", "coordinates": [ -68.878884056167365, -32.983540619870325 ] } },
+{ "type": "Feature", "properties": { "id": null, "Nombre": "El Club de la Milanesa (Chacras)" }, "geometry": { "type": "Point", "coordinates": [ -68.875791281585606, -32.972944372886346 ] } },
+{ "type": "Feature", "properties": { "id": null, "Nombre": "Chachingo (Palmares)" }, "geometry": { "type": "Point", "coordinates": [ -68.858817874317765, -32.955034821474705 ] } },
+{ "type": "Feature", "properties": { "id": null, "Nombre": "Montecatini (Centro)" }, "geometry": { "type": "Point", "coordinates": [ -68.8426121566864, -32.884473306822869 ] } },
+{ "type": "Feature", "properties": { "id": null, "Nombre": "Marchigiana (Centro)" }, "geometry": { "type": "Point", "coordinates": [ -68.841955504453949, -32.88482556341404 ] } }
+]
+}
