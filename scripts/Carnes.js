ea473d7c9d9a1f17c70f107e48d34c0d63d8981d
nach00z <62139114+nach00z@users.noreply.github.com>
2020-10-02 12:29:18 -0300

    Add files via upload

---
+++ scripts/Carnes.js
@@ -0,0 +1,20 @@
+var Carnes ={
+"type": "FeatureCollection",
+"name": "Carnes",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": null, "Nombre": "Bardot" }, "geometry": { "type": "Point", "coordinates": [ -68.849305989138045, -32.8877757698802 ] } },
+{ "type": "Feature", "properties": { "id": null, "Nombre": "El Club de la Milanesa (Arístides)" }, "geometry": { "type": "Point", "coordinates": [ -68.856708642256933, -32.891933786600845 ] } },
+{ "type": "Feature", "properties": { "id": null, "Nombre": "Torito" }, "geometry": { "type": "Point", "coordinates": [ -68.852036934938496, -32.883896493095378 ] } },
+{ "type": "Feature", "properties": { "id": null, "Nombre": "El Asadito" }, "geometry": { "type": "Point", "coordinates": [ -68.856224050139303, -32.883777925703562 ] } },
+{ "type": "Feature", "properties": { "id": null, "Nombre": "Josefina Restó" }, "geometry": { "type": "Point", "coordinates": [ -68.852305674172086, -32.892727509154298 ] } },
+{ "type": "Feature", "properties": { "id": null, "Nombre": "Cordillera Vinos y Fuegos" }, "geometry": { "type": "Point", "coordinates": [ -68.850093062458754, -32.88947621572747 ] } },
+{ "type": "Feature", "properties": { "id": null, "Nombre": "Tucson Steak House&bar" }, "geometry": { "type": "Point", "coordinates": [ -68.822791980119447, -32.916021203354624 ] } },
+{ "type": "Feature", "properties": { "id": null, "Nombre": "Dantesco" }, "geometry": { "type": "Point", "coordinates": [ -68.822550760932018, -32.915960133178821 ] } },
+{ "type": "Feature", "properties": { "id": null, "Nombre": "Casa Vigil (Palmares)" }, "geometry": { "type": "Point", "coordinates": [ -68.859212248545148, -32.955219557349508 ] } },
+{ "type": "Feature", "properties": { "id": null, "Nombre": "Club de la Milanesa (Palmares)" }, "geometry": { "type": "Point", "coordinates": [ -68.858875307457936, -32.955505495594259 ] } },
+{ "type": "Feature", "properties": { "id": null, "Nombre": "El Faro Bistro" }, "geometry": { "type": "Point", "coordinates": [ -68.842493461530523, -32.891793929940427 ] } },
+{ "type": "Feature", "properties": { "id": null, "Nombre": "Dantesco (Chacras)" }, "geometry": { "type": "Point", "coordinates": [ -68.878884056167365, -32.983540619870325 ] } },
+{ "type": "Feature", "properties": { "id": null, "Nombre": "El Club de la Milanesa (Chacras)" }, "geometry": { "type": "Point", "coordinates": [ -68.875791281585606, -32.972944372886346 ] } }
+]
+}
---
+++ scripts/Vegano.js
@@ -0,0 +1,14 @@
+var Vegano ={
+"type": "FeatureCollection",
+"name": "Vegano",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": null, "Nombre": "Govinda (Centro)" }, "geometry": { "type": "Point", "coordinates": [ -68.845869572935641, -32.894045975409696 ] } },
+{ "type": "Feature", "properties": { "id": null, "Nombre": "Mumbai Masala" }, "geometry": { "type": "Point", "coordinates": [ -68.848772818155737, -32.892340211155762 ] } },
+{ "type": "Feature", "properties": { "id": null, "Nombre": "Caléndula" }, "geometry": { "type": "Point", "coordinates": [ -68.857688355900592, -32.88951067179844 ] } },
+{ "type": "Feature", "properties": { "id": null, "Nombre": "Govinda (Godoy Cruz)" }, "geometry": { "type": "Point", "coordinates": [ -68.845849471336692, -32.914808055470473 ] } },
+{ "type": "Feature", "properties": { "id": null, "Nombre": "Cuenco Vegano" }, "geometry": { "type": "Point", "coordinates": [ -68.853072645893633, -32.884699689115855 ] } },
+{ "type": "Feature", "properties": { "id": null, "Nombre": "Tahiel Vegano" }, "geometry": { "type": "Point", "coordinates": [ -68.846651620856832, -32.918993016928553 ] } },
+{ "type": "Feature", "properties": { "id": null, "Nombre": "Madero Resto Vegano" }, "geometry": { "type": "Point", "coordinates": [ -68.849596026493884, -32.887560816700031 ] } }
+]
+}
