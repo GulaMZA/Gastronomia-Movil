1a42e90f95881720036cec59584fb2b133136344
nach00z <62139114+nach00z@users.noreply.github.com>
2020-11-19 13:16:32 -0300

    Add files via upload

--- scripts/Vegano.js
+++ scripts/Vegano.js
@@ -9,7 +9,6 @@ var Vegano ={
 { "type": "Feature", "properties": { "id": null, "Nombre": "Govinda (Godoy Cruz)", "Descrip": "Restó y pastelería vegetariana y vegana. Pastas, hamburguesas, milanesas, pizzas y tortas." }, "geometry": { "type": "Point", "coordinates": [ -68.845849471336692, -32.914808055470473 ] } },
 { "type": "Feature", "properties": { "id": null, "Nombre": "Cuenco Vegano", "Descrip": "Restó vegano. Brunch, jugos, pizzas, hamburguesas y comidas al plato." }, "geometry": { "type": "Point", "coordinates": [ -68.853072645893633, -32.884699689115855 ] } },
 { "type": "Feature", "properties": { "id": null, "Nombre": "Tahiel Vegano", "Descrip": "Comida vegana, libre de gluten y sin TACC. Pastas, pizzas, empanadas, sandwiches, picadas y postres." }, "geometry": { "type": "Point", "coordinates": [ -68.846651620856832, -32.918993016928553 ] } },
-{ "type": "Feature", "properties": { "id": null, "Nombre": "Madero Resto Vegano", "Descrip": "Comida rápida vegetariana y vegana. Hamburguesas, panchos, tacos, nachos, pizzetas. Pastelería vegana." }, "geometry": { "type": "Point", "coordinates": [ -68.849596026493884, -32.887560816700031 ] } },
-{ "type": "Feature", "properties": { "id": null, "Nombre": null, "Descrip": null }, "geometry": null }
+{ "type": "Feature", "properties": { "id": null, "Nombre": "Madero Resto Vegano", "Descrip": "Comida rápida vegetariana y vegana. Hamburguesas, panchos, tacos, nachos, pizzetas. Pastelería vegana." }, "geometry": { "type": "Point", "coordinates": [ -68.849596026493884, -32.887560816700031 ] } }
 ]
 }
